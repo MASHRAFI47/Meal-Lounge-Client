f054049e41d00fd248deebba21f26a1bab4ce292
MASHRAFI47 <mbngms@gmail.com>
2024-06-04 20:25:53 +0600

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Root from "../layouts/Root";
+
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
+    },
+]);
