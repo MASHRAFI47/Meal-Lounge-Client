6d03b8753a78adc9fd0420357bdd52e69c5ce2be
MASHRAFI47 <mbngms@gmail.com>
2024-06-08 16:42:10 +0600

    modified meal details page

--- src/components/MealDetails/MealDetails.jsx
+++ src/components/MealDetails/MealDetails.jsx
@@ -4,11 +4,17 @@ import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import useAuth from "../../hooks/useAuth";
 
+import Rating from 'react-rating'
+
+import emptyStar from '../../assets/images/emptystar.png'
+import star from '../../assets/images/star.png'
+
 const MealDetails = () => {
     const { loading } = useAuth()
     const { id } = useParams();
     const axiosCommon = useAxiosCommon();
 
+    // eslint-disable-next-line no-unused-vars
     const { data: meal = [], isLoading, refetch } = useQuery({
         queryKey: ['meal'],
         queryFn: async () => {
@@ -19,11 +25,43 @@ const MealDetails = () => {
 
     if (isLoading || loading) return <LoadingSpinner />
 
-    console.log(meal)
 
-    console.log(id)
     return (
-        <div>MealDetails</div>
+        <div className="relative top-40">
+            <div className="container mx-auto mb-10">
+                <div className="">
+                    <div className="space-y-2 my-5">
+                        <h1 className="text-3xl"><span className="font-bold">Item:</span> {meal?.title}</h1>
+                        <h1 className="text-2xl"><span className="font-semibold">Description:</span> {meal?.description}</h1>
+                        <h3 className="text-2xl font-semibold text-red-600"><span>Price:</span> ${meal?.price}</h3>
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-4 mb-4">
+                    <div className="col-span-3">
+                        <img src={meal?.image} className="rounded-lg w-full" alt="" />
+                    </div>
+                    <div className="ml-0 md:ml-5 space-y-2">
+                        <h3 className="text-2xl"><span className="font-semibold">Distributor:</span> {meal?.adminName}</h3>
+                        <h3 className="text-2xl"><span className="font-semibold">Ingredients:</span> {meal?.ingredients}</h3>
+                        <h3 className="text-2xl"><span className="font-semibold">Rating:</span>
+                            <Rating
+                                emptySymbol={<img src={emptyStar} className="icon w-4 mr-1" />}
+                                fullSymbol={<img src={star} className="icon w-4" />}
+                                initialRating={meal?.rating}
+                                readonly
+                            />
+                        </h3>
+                        <h3 className="text-2xl"><span className="font-semibold">Likes:</span> {meal?.likes}</h3>
+                        <button className="btn bg-[#CA301B] hover:bg-[#ff3535] text-white">Request</button>
+                    </div>
+                </div>
+
+                <div className="mt-10">
+                    <p className="text-xl"> <span className="font-semibold">Reviews:</span> {meal?.reviews}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
