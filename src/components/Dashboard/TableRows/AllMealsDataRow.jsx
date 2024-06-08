69072a827ae15f54e1a54fec0066337bde248e07
MASHRAFI47 <mbngms@gmail.com>
2024-06-08 16:19:45 +0600

    data table fixes

---
+++ src/components/Dashboard/TableRows/AllMealsDataRow.jsx
@@ -0,0 +1,41 @@
+import PropTypes from 'prop-types';
+
+
+const AllMealsDataRow = ({ meal }) => {
+    return (
+        <tr>
+            <td>
+                <div className="flex items-center gap-3">
+                    <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                            <img src={meal?.image} alt="Avatar Tailwind CSS Component" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{meal?.title}</div>
+                        <div className="text-sm opacity-50">{meal?.likes} likes</div>
+                    </div>
+                </div>
+            </td>
+            <td>{meal?.adminName}</td>
+            <td className='w-96'>
+                {meal?.reviews}
+                {/* <br />
+                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span> */}
+            </td>
+            <th>
+                <button className="btn btn-xs">Update</button>
+                <button className="btn btn-xs">Delete</button>
+                <button className="btn btn-xs">View</button>
+            </th>
+        </tr>
+    )
+}
+
+AllMealsDataRow.propTypes = {
+    meal: PropTypes.object,
+}
+
+
+
+export default AllMealsDataRow
--- src/pages/Dashboard/Admin/AllMeals.jsx
+++ src/pages/Dashboard/Admin/AllMeals.jsx
@@ -1,7 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import AllMealsDataRow from '../../../components/Dashboard/TableRows/AllMealsDataRow';
+import LoadingSpinner from '../../../components/LoadingSpinner/LoadingSpinner';
+
 
 const AllMeals = () => {
+  const axiosSecure = useAxiosSecure()
+
+  const { data: meals = [], isLoading } = useQuery({
+    queryKey: ['meals'],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get('/meals')
+      return data
+    }
+  })
+
+  if (isLoading) return <LoadingSpinner />
+
   return (
-    <div>AllMeals</div>
+    <div className=''>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Distributor</th>
+              <th>Reviews</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {
+              meals?.map(meal => <AllMealsDataRow key={meal._id} meal={meal} />)
+            }
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
